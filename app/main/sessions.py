118f79e80216126157197c05bd05195c03e6acce
gcatanese <gcatanese@yahoo.com>
2021-12-13 11:20:01 +0100

    Add sessions call

---
+++ app/main/sessions.py
@@ -0,0 +1,36 @@
+import app.main.config as config
+import Adyen
+import json
+
+'''
+Create Payment Session by calling /sessions
+
+Request must provide few mandatory attributes (merchantAccount, amount, returnUrl, transaction reference)
+
+Your backend should have a payment state where you can fetch information like amount and shopperReference
+'''
+
+
+def adyen_sessions():
+    adyen = Adyen.Adyen()
+    adyen.client.platform = 'test'
+    adyen.client.xapikey = config.checkout_apikey
+    adyen.client.app_name = 'myapp'
+
+    request = {}
+
+    request['amount'] = {"value": "1000", "currency": "EUR"}
+    request['reference'] = "YOUR_PAYMENT_REFERENCE"
+    request['merchantAccount'] = config.merchant_account
+    request['returnUrl'] = "https://your-company.com/checkout?shopperOrder=12xy.."
+    request['countryCode'] = "NL"
+
+    result = adyen.checkout.sessions(request)
+
+    print("/result:\n" + str(result))
+
+    formatted_response = json.dumps((json.loads(result.raw_response)))
+    # formatted_response = json.dumps((json.loads(result)))
+    print("/sessions response:\n" + formatted_response)
+
+    return formatted_response
