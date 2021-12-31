7510234d422dad830cb59244b128fbfddca46066
gcatanese <gcatanese@yahoo.com>
2021-12-31 11:08:06 +0100

    Add HMAC key configuration

--- app/main/config.py
+++ app/main/config.py
@@ -9,12 +9,13 @@ Make sure to fill out your config.ini file!!!
 merchant_account = ""
 checkout_apikey = ""
 client_key = ""
+hmac_key = ""
 supported_integrations = ['dropin', 'card', 'ideal', 'klarna', 'directEbanking', 'alipay', 'boletobancario',
                           'sepadirectdebit', 'dotpay', 'giropay', 'ach', 'paypal', 'applepay']
 
 
 def read_config():
-    global merchant_account, checkout_apikey, client_key
+    global merchant_account, checkout_apikey, client_key, hmac_key
 
     config = configparser.ConfigParser(interpolation=None)
     config.read('config.ini')
@@ -22,6 +23,7 @@ def read_config():
     merchant_account = config['DEFAULT']['merchant_account']
     checkout_apikey = config['DEFAULT']['apikey']
     client_key = config['DEFAULT']['client_key']
+    hmac_key = config['DEFAULT']['hmac_key']
 
     # Check to make sure variables are set
     if not merchant_account or not checkout_apikey or not client_key:
